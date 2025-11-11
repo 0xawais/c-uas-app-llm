2daff1a16252f2c9ce84144a46758021d68e281b
Luke <lnalewaj@andrew.cmu.edu>
2025-11-11 15:33:18 -0500

    changes to main

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,7 @@
 import { Routes } from '@angular/router';
 import { ProductReviewComponent } from './product-review/product-review';
-<<<<<<< HEAD
 import { NavSystem } from './nav-system/nav-system';
-
-=======
 import { ReviewSystem } from './review-system/review-system';
->>>>>>> luke
 
 export const routes: Routes = [
     { 
