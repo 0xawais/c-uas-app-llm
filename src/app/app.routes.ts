1c101891fb168d49256f3f0339ecf445b883890b
Luke <lnalewaj@andrew.cmu.edu>
2025-11-11 14:29:36 -0500

    add review system page/feedback form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { ProductReviewComponent } from './product-review/product-review';
+import { ReviewSystem } from './review-system/review-system';
 
 export const routes: Routes = [
     { 
         path: 'product-review', 
         component: ProductReviewComponent
+    },
+    { 
+        path: 'review-system', 
+        component: ReviewSystem
     }
 ];
